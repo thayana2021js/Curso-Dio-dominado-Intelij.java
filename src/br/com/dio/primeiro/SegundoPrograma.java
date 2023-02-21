461251eabb6fd33cdcdd2d865a3341e4af74a891
Thayana Vilaça Fortunato <athay.thayana@gmail.com>
2023-02-21 13:25:27 -0300

    Criando class Calc005

--- src/br/com/dio/primeiro/SegundoPrograma.java
+++ src/br/com/dio/primeiro/SegundoPrograma.java
@@ -2,23 +2,7 @@ package br.com.dio.primeiro;
 
 import model.Gato;
 
-public class SegundoPrograma {
-
-    public static void main(String[] args) {
-        /*int a = 5;
-        int b = 3;
-        System.out.println(" HELLO WORLD THAYANA ");*/
-
-        Gato gato = new Gato();
-        System.out.println(gato);
-
-        Animal animal01 = new Animal(nome" Cachorro", raca indefinida);
-        System.out.println(raca);
-
-    }
-}
-
- class  Animal{
+class  Animal{
     private String nome;
     private String raca;
 
